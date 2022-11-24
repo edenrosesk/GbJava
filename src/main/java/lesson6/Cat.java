e80bf290986dbf189f9439987b125ff5de8cfc1e
edenrosesk <edenrose.sk@gmail.com>
2022-11-24 15:24:50 +0300

    Homework6

---
+++ src/main/java/lesson6/Cat.java
@@ -0,0 +1,10 @@
+package lesson6;
+
+public class Cat extends Animal{
+
+
+    protected Cat(String name) {
+        super(name, 200, 0);
+    }
+
+}
---
+++ src/main/java/lesson6/Dog.java
@@ -0,0 +1,13 @@
+package lesson6;
+
+public class Dog extends Animal {
+
+
+    protected Dog(String name) {
+        super(name, 500, 10);
+
+    }
+
+
+
+}
